54d66e065e611b3f24faaa08cb9c35452f743892
Abhinai Reddy⛔️ <159715958+abhinai2244@users.noreply.github.com>
2025-04-06 23:28:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8086307114:AAEXFmrgRecAHokGduJKr98-nJvley_09Gs")
+API_ID = int(os.environ.get("API_ID", "29483517"))
+API_HASH = os.environ.get("API_HASH", "e35a05d338376cbcd8162f810aed878d")
+OWNER = int(os.environ.get("OWNER", "5756495153"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "rtxx_renamerbot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "anime_rtxx")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002616517280"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://user1:abhinai.2244@cluster0.7oaqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQHB4f0AvFmQm7HK3eVLKg1bwfcF2K88FmqPmFnm7ov8Y7JwZNuIrWJOqOGh2itLbYo-1JFzHf4EDVwDmhMXUVZVTtAhCBHn4iYon7TbxUS7bDSuuha1VbWZd4kCOZrkL3o6L26I8h29uphpEdD8cWWztUp2djKFJN9xmbrHzPPtZ64hbFCCkVxnPijoC08BoDbwBljkChuwK4kDwH_JafoCNZhdo8cR4RPTSE-sHVrEPWirMiWXrz6b-lz28iYZOYTrx7-qhYuhrPPypw7YEdlHyTw-KB7zhngjalm0hbCeafvaHtOzcgCGWEKdfEk_1CL9pgL4OuRpH1CObF9TEWZN1X1ltgAAAAFXHSUxAA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
 
